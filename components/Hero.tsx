c8409105bf1033ed827968205ed058ae4d9dba62
Ben King <benking1293@gmail.com>
2023-07-22 16:30:51 +0100

    Added car image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book or rent a car — quickly and easily!
+          Find, book or rent a car — quickly and super easily!
         </h1>
         <p className="hero__subtitle">
           Streamline your car rental experience with out effortless booking
@@ -21,6 +21,12 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   )
